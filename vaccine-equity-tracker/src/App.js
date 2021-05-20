8caf3dbdbfe61f4b4c86f4bd2cf85e50a358dae6
jmwang00 <83558210+jmwang00@users.noreply.github.com>
2021-05-20 16:47:15 -0700

    Update App.js
    Implement functionality for map buttons.

--- vaccine-equity-tracker/src/App.js
+++ vaccine-equity-tracker/src/App.js
@@ -4,14 +4,28 @@ import WorldMap from './components/WorldMap'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 
-function App() { 
-  return (
-    <div className="App">
-      <NavBar/>
-      <WorldMap/>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component { 
+  constructor(props){
+    super(props);
+    this.state = {
+        data: 0
+    }
+  }
+
+  handleCallback = (childData) =>{
+    this.setState({data: childData})
+  }
+
+  render() {
+    const {data} = this.state;
+      return (
+      <div className="App">
+        <NavBar/>
+        <WorldMap dataParentToChild = {data}/>
+        <Footer parentCallback = {this.handleCallback}/>
+      </div>
+    );
+    }
 }
 
 export default App;
