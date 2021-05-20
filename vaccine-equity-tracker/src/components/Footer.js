681e56a863c6ff17d0784b1d3f3794e32d0cb2be
jmwang00 <83558210+jmwang00@users.noreply.github.com>
2021-05-20 16:47:48 -0700

    Implement functionality for map buttons.

--- vaccine-equity-tracker/src/components/Footer.js
+++ vaccine-equity-tracker/src/components/Footer.js
@@ -5,28 +5,24 @@ import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-var data = 1;
-
-function setValue(val) {
-  data = val;
-  console.log(data);
-}
-
-function Footer() {
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Nav className="mr-auto">
-        <Navbar.Brand href="#home">
-          CS52
-        </Navbar.Brand>
-      </Nav>
-      <ButtonGroup aria-label="Basic example" className="mr-sm-2">
-          <Button variant="secondary" onClick={() => setValue(1)}>Vaccination Rate</Button>
-          <Button variant="secondary" onClick={() => setValue(2)}>GDP</Button>
-          <Button variant="secondary" onClick={() => setValue(3)}>Life Expectancy</Button>
-      </ButtonGroup>
-    </Navbar>
-  );
+class Footer extends React.Component{
+  
+  render() {
+      return (
+      <Navbar bg="dark" variant="dark">
+        <Nav className="mr-auto">
+          <Navbar.Brand href="#home">
+            CS52
+          </Navbar.Brand>
+        </Nav>
+        <ButtonGroup aria-label="Basic example" className="mr-sm-2">
+            <Button variant="secondary" onClick={() => this.props.parentCallback(0)}>Vaccination Rate</Button>
+            <Button variant="secondary" onClick={() => this.props.parentCallback(1)}>GDP</Button>
+            <Button variant="secondary" onClick={() => this.props.parentCallback(2)}>Life Expectancy</Button>
+        </ButtonGroup>
+      </Navbar>
+    );
+  }
 }
 
 export default Footer;
